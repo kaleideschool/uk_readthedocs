5ece1996dc400ee6bf2fae3945e7552c34d23188
Diego <ground.contact@gmail.com>
2020-01-27 21:35:42 +0000

    Formating Latex

--- conf.py
+++ conf.py
@@ -57,3 +57,11 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+ 
+ #Pdf generation
+
+latex_engine = 'xelatex'
+latex_documents = ('index', 'test', 'TEST', 'Diana')
+latex_show_pagerefs = True
+
